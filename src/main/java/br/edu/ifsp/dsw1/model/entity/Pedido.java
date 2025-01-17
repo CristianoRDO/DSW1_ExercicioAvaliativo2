e532bfb963c2fd499cb6c645fc83d35e45e720de
CristianoRDO <crisrooliveira.2004@gmail.com>
2025-01-17 15:24:13 -0300

    Implementação de Entidade Pedido

--- src/main/java/br/edu/ifsp/dsw1/model/entity/Pedido.java
+++ src/main/java/br/edu/ifsp/dsw1/model/entity/Pedido.java
@@ -1,5 +1,57 @@
 package br.edu.ifsp.dsw1.model.entity;
 
 public class Pedido {
-
+	
+	private int idProduto;
+	private String nomeCliente;
+	private String endereco;
+	private String descricao;
+	private double valor;
+	
+	public Pedido(){}
+	
+	public Pedido(int idProduto, String nomeCliente, String endereco, String descricao, double valor) {
+		this.idProduto = idProduto;
+		this.nomeCliente = nomeCliente;
+		this.endereco = endereco;
+		this.descricao = descricao;
+		this.valor = valor;
+	}
+	
+	public int getIdProduto() {
+		return idProduto;
+	}
+	public void setIdProduto(int idProduto) {
+		this.idProduto = idProduto;
+	}
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	
+	
+	
+	
+	
 }
