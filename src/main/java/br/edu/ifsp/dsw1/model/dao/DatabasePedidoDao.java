1176e4703f0cc128c400e3aadae45e1beff98da2
CristianoRDO <crisrooliveira.2004@gmail.com>
2025-01-17 16:50:29 -0300

    Refatorando Entidade Pedido - Sobrecarga de Construtor para Permitir Criação de Pedidos sem ID

--- src/main/java/br/edu/ifsp/dsw1/model/dao/DatabasePedidoDao.java
+++ src/main/java/br/edu/ifsp/dsw1/model/dao/DatabasePedidoDao.java
@@ -45,31 +45,26 @@ public class DatabasePedidoDao implements PedidoDao{
 
 	@Override
 	public boolean update(int id, Pedido updatedPedido) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean delete(int id) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public List<Pedido> retrieve(User user) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public List<Pedido> retrieveFindByName(User user, String name) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Pedido findById(int id) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
--- src/main/java/br/edu/ifsp/dsw1/model/entity/Pedido.java
+++ src/main/java/br/edu/ifsp/dsw1/model/entity/Pedido.java
@@ -10,12 +10,17 @@ public class Pedido {
 	
 	public Pedido(){}
 	
-	public Pedido(int idPedido, String nomeCliente, String endereco, String descricao, double valor) {
-		this.idPedido = idPedido;
+	public Pedido(String nomeCliente, String endereco, String descricao, double valor) {
 		this.nomeCliente = nomeCliente;
 		this.endereco = endereco;
 		this.descricao = descricao;
 		this.valor = valor;
+	}
+	
+	// Permitir a criação de Objetos do Tipo pedido antes de armazenalos no banco (id gerado automaticamente durante a inserção).
+	public Pedido(int idPedido, String nomeCliente, String endereco, String descricao, double valor) {
+		this(nomeCliente, endereco, descricao, valor);
+		this.idPedido = idPedido;
 	}
 	
 	public int getIdPedido() {
