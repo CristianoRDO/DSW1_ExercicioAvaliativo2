4db676a9ae178aff62464a57d992e3a0925de584
CristianoRDO <crisrooliveira.2004@gmail.com>
2025-01-18 23:05:48 -0300

    Removendo Pool de Conexões

--- src/main/java/br/edu/ifsp/dsw1/model/dao/connection/DatabaseConnection.java
+++ src/main/java/br/edu/ifsp/dsw1/model/dao/connection/DatabaseConnection.java
@@ -1,28 +1,27 @@
 package br.edu.ifsp.dsw1.model.dao.connection;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
-
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
 
 public class DatabaseConnection {
 
-	private DatabaseConnection() {
-	}
-
-	private static final String RESOURCE = "java:/comp/env/jdbc/mysql";
-
-	public static Connection getConnection() throws SQLException {
+	private DatabaseConnection() { }
+	
+	private static final String URL = "jdbc:mysql://localhost:3307/pedidos_db";
+	private static final String USER = "root";
+	private static final String PASSWORD = "root";
+	
+	static {
 		try {
-			InitialContext context = new InitialContext();
-			DataSource dataSource = (DataSource) context.lookup(RESOURCE);
-			return dataSource.getConnection();
-		} catch (NamingException e) {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-		return null;
+	}
+	
+	public static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(URL, USER, PASSWORD);
 	}
 
 }
